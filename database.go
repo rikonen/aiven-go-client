d987584f6e72f959d441b349a2663a4e00002ba3
Rauli Ikonen <rauli@aiven.io>
2018-09-19 18:54:07 +0300

    Fix database APIs

--- database.go
+++ database.go
@@ -1,11 +1,17 @@
 package aiven
 
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+)
+
 type (
 	// Database represents a database type on Aiven.
 	Database struct {
-		Database  string `json:"database"`
-		LcCollate string `json:"lc_collate,omitempty"`
-		LcType    string `json:"lc_type,omitempty"`
+		DatabaseName string `json:"database_name"`
+		LcCollate    string `json:"lc_collate,omitempty"`
+		LcType       string `json:"lc_ctype,omitempty"`
 	}
 
 	// DatabasesHandler is the client which interacts with the Aiven database
@@ -16,9 +22,16 @@ type (
 
 	// CreateDatabaseRequest are the parameters used to create a database.
 	CreateDatabaseRequest struct {
-		Database  string `json:"database"`
-		LcCollate string `json:"lc_collate,omitempty"`
-		LcType    string `json:"lc_type,omitempty"`
+		DatabaseName string `json:"database"`
+		LcCollate    string `json:"lc_collate,omitempty"`
+		LcType       string `json:"lc_ctype,omitempty"`
+	}
+
+	// DatabaseListResponse represents the response from Aiven for listing
+	// databases.
+	DatabaseListResponse struct {
+		APIResponse
+		Databases []*Database `json:"databases"`
 	}
 )
 
@@ -43,6 +56,25 @@ func (h *DatabasesHandler) Create(project, service string, req CreateDatabaseReq
 	return &db, nil
 }
 
+// Get a specific database from Aiven.
+func (h *DatabasesHandler) Get(projectName, serviceName, databaseName string) (*Database, error) {
+	// There's no API for getting database by name. List all databases and pick the correct one
+	// instead. (There typically aren't that many databases, 100 is already very large number)
+	databases, err := h.List(projectName, serviceName)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, database := range databases {
+		if database.DatabaseName == databaseName {
+			return database, nil
+		}
+	}
+
+	err = Error{Message: fmt.Sprintf("Database with name %v not found", databaseName), Status: 404}
+	return nil, err
+}
+
 // Delete deletes the specified database.
 func (h *DatabasesHandler) Delete(project, service, database string) error {
 	path := buildPath("project", project, "service", service, "db", database)
@@ -52,4 +84,24 @@ func (h *DatabasesHandler) Delete(project, service, database string) error {
 	}
 
 	return handleDeleteResponse(bts)
+}
+
+// List will fetch all databases for a given service.
+func (h *DatabasesHandler) List(project, service string) ([]*Database, error) {
+	path := buildPath("project", project, "service", service, "db")
+	rsp, err := h.client.doGetRequest(path, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var response *DatabaseListResponse
+	if err := json.Unmarshal(rsp, &response); err != nil {
+		return nil, err
+	}
+
+	if len(response.Errors) != 0 {
+		return nil, errors.New(response.Message)
+	}
+
+	return response.Databases, nil
 }
